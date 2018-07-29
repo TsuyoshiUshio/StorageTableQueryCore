055fb68c0b145d18b30809e696ed2b3df0734619
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-30 06:34:04 +0900

    Add project files.

---
+++ TableQuerySpike/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TableQuerySpike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
