61e1399fba048e4de44d041acfd55686e0548d5f
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-07-31 08:16:11 +0900

    Adding combinatino testing.

--- TableQuerySpike.Test/OrchestrationInstanceStatusQueryBuilderTest.cs
+++ TableQuerySpike.Test/OrchestrationInstanceStatusQueryBuilderTest.cs
@@ -19,7 +19,6 @@ namespace TableQuerySpike.Test
         {
             var queryBuilder = new OrchestrationInstanceStatusQuerBuilder();
             var createdDateFrom =  new DateTime(2018, 1, 10, 10, 10, 10);
-            Console.WriteLine(createdDateFrom.ToString());
             var createdDateTo = new DateTime(2018, 1, 10, 10, 10, 50);
 
             queryBuilder.AddCreatedDate(createdDateFrom, createdDateTo);
@@ -33,9 +32,27 @@ namespace TableQuerySpike.Test
         {
             var queryBuilder = new OrchestrationInstanceStatusQuerBuilder();
             var createdDateFrom = new DateTime(2018, 1, 10, 10, 10, 10);
-            Console.WriteLine(createdDateFrom.ToString());
             queryBuilder.AddCreatedDate(createdDateFrom, default(DateTime));
             Assert.Equal("CreatedDate ge datetime'2018-01-10T01:10:10.0000000Z'", queryBuilder.Build().FilterString);
+
+            var createdDateTo = new DateTime(2018, 1, 10, 10, 10, 50);
+
+            queryBuilder.AddCreatedDate(default(DateTime), createdDateTo);
+            Assert.Equal("CreatedDate le datetime'2018-01-10T01:10:50.0000000Z'", queryBuilder.Build().FilterString);
         }        
+
+        [Fact]
+        public void Test_OrchestrationInstanceQuery_Combination()
+        {
+            var queryBuilder = new OrchestrationInstanceStatusQuerBuilder();
+            queryBuilder.AddRuntimeStatus("Runnning");
+            var createdDateFrom = new DateTime(2018, 1, 10, 10, 10, 10);
+            var createdDateTo = new DateTime(2018, 1, 10, 10, 10, 50);
+
+            queryBuilder.AddCreatedDate(createdDateFrom, createdDateTo);
+            var result = queryBuilder.Build().FilterString;
+            Assert.Equal("((CreatedDate ge datetime'2018-01-10T01:10:10.0000000Z') and (CreatedDate le datetime'2018-01-10T01:10:50.0000000Z')) and (RuntimeStatus eq 'Runnning')", queryBuilder.Build().FilterString);
+
+        }
     }
 }
